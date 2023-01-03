9dd404c74524ca7c71bbef6ab58641e9418d960b
Eron Alves <eron.a.silva@capgemini.com>
2023-01-03 00:24:32 -0300

    refactor: extract string literal values to constants

--- src/main/java/com/eronalves/peoplemanager/service/PersonServiceImpl.java
+++ src/main/java/com/eronalves/peoplemanager/service/PersonServiceImpl.java
@@ -12,6 +12,8 @@ import com.eronalves.peoplemanager.repositories.PersonRepository;
 @Service
 public class PersonServiceImpl implements PersonService {
 
+    private static final String PERSON_NOT_FINDED_MSG = "Person not finded";
+    private static final String INCONSISTENT_ID_MSG = "Inconsistent Id information";
     private PersonRepository repository;
 
     public PersonServiceImpl(PersonRepository repository) {
@@ -26,21 +28,18 @@ public class PersonServiceImpl implements PersonService {
     @Override
     public Person updatePersonById(int id, Person person) throws Exception {
         if(person.getId() == null) person.setId(id);
-        if(person.getId() != id) throw new Exception("Inconsistent Id information");
+        if(person.getId() != id) throw new Exception(INCONSISTENT_ID_MSG);
         return persistOnDatabase(person);
     }
 
-    public Person persistOnDatabase(Person person){
-        return repository.save(person);
-    }
-
+    
     @Override
     public Person getPersonById(int id) throws Exception {
         Optional<Person> personOpt = repository.findById(id);
         if(personOpt.isPresent()) return personOpt.get();
-        throw new Exception("Person not finded");
+        throw new Exception(PERSON_NOT_FINDED_MSG);
     }
-
+    
     @Override
     public List<Person> getAllPersons() {
         Iterable<Person> personIterable = repository.findAll();
@@ -48,5 +47,8 @@ public class PersonServiceImpl implements PersonService {
         personIterable.forEach(personList::add);
         return personList;
     }
-
+    
+    private Person persistOnDatabase(Person person){
+        return repository.save(person);
+    }
 }
